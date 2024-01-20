477694594eb882ec201f387dd41a05c6093d5fbd
Tony Nadeau <thegoldenrailway@gmail.com>
2024-01-20 18:36:59 -0500

    Fixed android loading error

--- tuna/lib/main.dart
+++ tuna/lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+
 void main() {
+  Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
@@ -70,12 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
-  }
-
-  void _initializeFirebase() async {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
   }
 
   @override
